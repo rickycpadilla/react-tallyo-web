a73ebdada8f0ca6a8ea48d34b37bd139f08aeab7
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-19 15:26:37 -0600

    styling complete, but will need to make more modular eventually

--- app/index.js
+++ app/index.js
@@ -1,8 +1,29 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-// var routes = require('./config/routes')
+var bg = require('./styles').Bg;
+
+var Home = React.createClass({
+  render: function(){
+    return (
+      <div className="container">
+
+
+        <div className="circle">
+          <img className="minus" src="./images/minus.png" />
+          <div className="numTexty">
+            <div className="num">0</div>
+            <div className="texty">tallyo</div>
+          </div>
+          <img className="plus" src="./images/plus.png" />
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = Home;
 
 ReactDOM.render(
   // routes, document.getElementById('app')
-  <p>Hellow world</p>, document.getElementById('app')
+  <Home />, document.getElementById('app')
 );
---
+++ app/styles/index.js
@@ -0,0 +1,7 @@
+var styles = {
+  Bg: {
+    backgroundColor: 'yellow', height: 100, width:100
+  }
+};
+
+module.exports = styles;

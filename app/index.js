164af7c6a59f51dae9b3d027b79b2db044f026f4
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-19 17:23:29 -0600

    fully functional, needs refactored

--- app/index.js
+++ app/index.js
@@ -3,18 +3,31 @@ var ReactDOM = require('react-dom');
 var bg = require('./styles').Bg;
 
 var Home = React.createClass({
+  getInitialState : function() {
+    return {
+      counter : 0
+    };
+  },
+  plusClicked: function(){
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+  minusClicked: function(){
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  },
   render: function(){
     return (
       <div className="container">
-
-
         <div className="circle">
-          <img className="minus" src="./images/minus.png" />
+          <img className="minus" onClick={this.minusClicked} src="./images/minus.png" />
           <div className="numTexty">
-            <div className="num">0</div>
+            <div className="num">{this.state.counter}</div>
             <input type="text" className="texty" placeholder="tallyo"/>
           </div>
-          <img className="plus" src="./images/plus.png" />
+          <img className="plus" onClick={this.plusClicked} src="./images/plus.png" />
         </div>
       </div>
     )
